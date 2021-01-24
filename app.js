86dbbc8150c3b7057cb1864a21ed22e31e4d5eb6
mahxmoud <mahmoud.jomaa@yahoo.com>
2021-01-24 14:45:01 +0200

    Created and Saved a Record of a Model

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+// REQUIRING THE MODEL
 const Person = require('./models/Person')
 
 const app = express()
 
-
+// CONNECTING TO THE DB
 mongoose.connect(
     process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -14,6 +15,20 @@ mongoose.connect(
 },
     console.log('db is connected')
 )
+
+// Create and Save a Record of a Model 
+let mahmoud = new Person({ name: "Mahmoud Joma", age: 24, favoriteFoods: ['Burger', 'Fries', 'Fast Food', 'anything not healthy'] })
+
+mahmoud.save((data, err) => {
+    try {
+        console.log(data)
+    } catch (err) {
+        console.log({ message: err })
+    }
+})
+
+
+
 
 // LISTENING TO THE SERVER ON 3000 PORT
 app.listen(3000, (err) => {
